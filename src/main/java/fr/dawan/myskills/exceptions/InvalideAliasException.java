b5c4b70f94671ec3343876bd191e0ed104a8fb24
Sukushi <romain.chuche@gmail.com>
2024-05-14 10:45:53 +0200

    alias generator

--- src/main/java/fr/dawan/myskills/exceptions/InvalideAliasException.java
+++ src/main/java/fr/dawan/myskills/exceptions/InvalideAliasException.java
@@ -2,10 +2,12 @@ package fr.dawan.myskills.exceptions;
 
 
 public class InvalideAliasException extends RuntimeException {
-
-    private static final long serialVersionUID = 1L;
-
-    public InvalideAliasException(String alias) {
-        super(String.format("Alias %s est incorrect", alias));
+	
+	public InvalideAliasException() {
+		super("Alias incorrect");
+	}
+    public InvalideAliasException(String error) {
+        super(String.format("Alias incorrect : %s", error));
     }
+	
 }
--- src/main/java/fr/dawan/myskills/tools/AliasGenerator.java
+++ src/main/java/fr/dawan/myskills/tools/AliasGenerator.java
@@ -1,23 +1,20 @@
 package fr.dawan.myskills.tools;
 
-import fr.dawan.myskills.entities.AliasableEntity;
 import fr.dawan.myskills.exceptions.InvalideAliasException;
 import fr.dawan.myskills.repositories.AliasableRepository;
 
 import java.text.Normalizer;
 
 public class AliasGenerator {
-    /**
-     * création auto d'un alias (refactor d'un alias, création à partir d'un name)
-     * format : kebab-case_id
-     * longueur limitée (cf constante)
-     *
-     * @param aliasable
-     * @param repo
-     * @return
-     */
+    
     public static final int LIMITE = 30;
-
+	
+	/**
+	 * Création d'un alias de longueur limitée (cf constante)
+	 * @param name
+	 * @param repository
+	 * @return
+	 */
     public static String generateAlias(String name, AliasableRepository repository) {
 		if (!name.isEmpty()) {
 			return generate(name,repository);
@@ -29,43 +26,38 @@ public class AliasGenerator {
      * 1- normalise l'Alias
      * 2- réduit l'alias à LIMITE si trop long
      * 3- si alias déjà existant en bdd: ajoute un incrément (mon-alias_1, mon-alias_2,...)
-     *
-     * @param name   : Alias ou Name (si alias non renseigné)
+     * @param name   : nom à 'aliasiser'
      * @param repository : nécessaire pour vérifier si déjà présent en Bdd
-     * @return nouvel alias
+     * @return un alias normalisé
      */
     private static String generate(String name, AliasableRepository repository) {
-        String aliasGenerated = (normalizeAlias(name));
-        // pour ne pas avoir des alias trop long => taille limitée à LIMITE caracteres
-        if (aliasGenerated.length() > LIMITE) aliasGenerated = aliasGenerated.substring(0, LIMITE);
+        String aliasNormalize = (normalize(name));
+        // pour ne pas avoir des alias trop long => taille limitée avec la constante LIMITE
+        if (aliasNormalize.length() > LIMITE) aliasNormalize = aliasNormalize.substring(0, LIMITE);
         // si l'alias généré existe en bdd => ajout d'un chiffre
-        if (repository.findByAlias(aliasGenerated).isPresent()) {
+        if (repository.findByAlias(aliasNormalize).isPresent()) {
 			int increment = 1;
-			aliasGenerated += "_";
-			while (repository.findByAlias(aliasGenerated + increment).isPresent()) {
+			aliasNormalize += "_";
+			while (repository.findByAlias(aliasNormalize + increment).isPresent()) {
 				increment++;
 			}
-			aliasGenerated += increment;
+			aliasNormalize += increment;
         }
-        return aliasGenerated;
+        return aliasNormalize;
     }
 
     /**
-     * Normalise l'alias
-     * supprime caractères spéciaux
-     * supprime les majuscules
-     * kebab-case (snake_case_autorisé ou ecriture-hybride_1 autorisée)
-     *
-     * @param alias à normaliser
-     * @return alias normalisé
-	 * @throws InvalideAliasException si alias null ou constitué uniquement d'espaces ou de caractères spéciaux
+     * Normalise l'alias en supprimant les caractères spéciaux et les majuscules
+     * @param text le texte à normaliser
+     * @return texte normalisé
+	 * @throws InvalideAliasException si le texte est null ou constitué uniquement d'espaces ou de caractères spéciaux
      */
-    public static String normalizeAlias(String alias) {
-        if (alias == null || alias.trim().isEmpty()) throw new InvalideAliasException("null");
+    public static String normalize(String text) {
+        if (text == null || text.trim().isEmpty()) throw new InvalideAliasException("chaîne vide");
 
-        String aliasNormalized = Normalizer.normalize(alias, Normalizer.Form.NFKD) // Les caractères sont décomposés par équivalence canonique et de compatibilité, et sont réordonnés.
+        String aliasNormalized = Normalizer.normalize(text, Normalizer.Form.NFKD) // Les caractères sont décomposés par équivalence canonique et de compatibilité, et sont réordonnés.
 				.toLowerCase()
-				.replace(" ","_")
+				.replace(" ","_") // remplace les espaces par des _
                 .replaceAll("[\\n\"'.:?!&/<>|]", "-") // remplace : retour ligne . : | / < > ? ! & ' " en -
                 .replaceAll("[^a-z0-9-_]", "") // supprime caractères spéciaux autres que _ et -
                 .replaceAll("[-]{2,}", "-") // si plusieurs - à la suite => 1seul
@@ -74,7 +66,7 @@ public class AliasGenerator {
 
         // s'il n'y a que des caractères spéciaux
         if (aliasNormalized.replaceAll("[-_]", "").isEmpty())
-            throw new InvalideAliasException("Utilisez des caractères alphanumériques");
+            throw new InvalideAliasException("utilisez des caractères alphanumériques non spéciaux");
         return aliasNormalized;
     }
 }
