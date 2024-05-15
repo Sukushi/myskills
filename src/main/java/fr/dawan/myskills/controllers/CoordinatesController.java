2e2bc9b761e7d0255281188eac0d57bf1183760b
Sukushi <romain.chuche@gmail.com>
2024-05-15 17:06:33 +0200

    fix Coordinates Controller

--- src/main/java/fr/dawan/myskills/controllers/CoordinatesController.java
+++ src/main/java/fr/dawan/myskills/controllers/CoordinatesController.java
@@ -3,7 +3,11 @@ package fr.dawan.myskills.controllers;
 import fr.dawan.myskills.dtos.CoordinatesDto;
 import fr.dawan.myskills.generic.GenericController;
 import fr.dawan.myskills.services.CoordinatesService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/coords")
 public class CoordinatesController extends GenericController<CoordinatesDto, CoordinatesService> {
     public CoordinatesController(CoordinatesService service) {
         super(service);
