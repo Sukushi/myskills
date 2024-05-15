7c65863edf77cfcee82653ea991101601aa4ee87
Sukushi <romain.chuche@gmail.com>
2024-05-15 10:42:02 +0200

    remove useless import

--- src/main/java/fr/dawan/myskills/repositories/QuizRepository.java
+++ src/main/java/fr/dawan/myskills/repositories/QuizRepository.java
@@ -1,14 +1,11 @@
 package fr.dawan.myskills.repositories;
 
-import fr.dawan.myskills.entities.Question;
 import fr.dawan.myskills.entities.Quiz;
-import fr.dawan.myskills.entities.Theme;
 import fr.dawan.myskills.entities.enums.QuizStatusEnum;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
