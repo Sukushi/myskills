a1a827a1ff1b556e30b7f32d494cd37d72159094
Sukushi <romain.chuche@gmail.com>
2024-05-15 17:11:56 +0200

    push QuestionController

---
+++ src/main/java/fr/dawan/myskills/constants/ParamConstants.java
@@ -0,0 +1,5 @@
+package fr.dawan.myskills.constants;
+
+public class ParamConstants {
+	public static final int PAGE_SIZE = 20;
+}
---
+++ src/main/java/fr/dawan/myskills/controllers/QuestionController.java
@@ -0,0 +1,39 @@
+package fr.dawan.myskills.controllers;
+
+import fr.dawan.myskills.constants.ParamConstants;
+import fr.dawan.myskills.dtos.QuestionDto;
+import fr.dawan.myskills.generic.GenericController;
+import fr.dawan.myskills.services.QuestionService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static fr.dawan.myskills.constants.ParamConstants.PAGE_SIZE;
+
+@RestController
+@RequestMapping("/questions")
+public class QuestionController extends GenericController<QuestionDto, QuestionService> {
+	public QuestionController(QuestionService service) {
+		super(service);
+	}
+	
+	@GetMapping("/text/{text}")
+	public ResponseEntity<Page<QuestionDto>> findByTextContaining(@PathVariable String text, @PageableDefault(size = PAGE_SIZE) Pageable page) {
+		return ResponseEntity.ok(service.findByTextContaining(text,page));
+	}
+	
+	@GetMapping("/quiz/{quizId}")
+	public ResponseEntity<Page<QuestionDto>> findByQuizId(@PathVariable Long quizId, @PageableDefault(size = PAGE_SIZE) Pageable page) {
+		return ResponseEntity.ok(service.findByQuizId(quizId,page));
+	}
+	
+	@GetMapping("/theme/{themeId}")
+	public ResponseEntity<Page<QuestionDto>> findByThemeId(@PathVariable Long themeId, @PageableDefault(size = PAGE_SIZE) Pageable page) {
+		return ResponseEntity.ok(service.findByThemeId(themeId,page));
+	}
+}
