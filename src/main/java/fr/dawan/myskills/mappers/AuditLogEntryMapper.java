b847566242a6bf2ba9268850a6756ceaef55496e
Sukushi <romain.chuche@gmail.com>
2024-05-16 14:37:03 +0200

    add @Mapper on AuditLogEntryMapper

--- src/main/java/fr/dawan/myskills/mappers/AuditLogEntryMapper.java
+++ src/main/java/fr/dawan/myskills/mappers/AuditLogEntryMapper.java
@@ -3,6 +3,9 @@ package fr.dawan.myskills.mappers;
 import fr.dawan.myskills.dtos.AuditLogEntryDto;
 import fr.dawan.myskills.entities.AuditLogEntry;
 import fr.dawan.myskills.generic.GenericMapper;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface AuditLogEntryMapper extends GenericMapper<AuditLogEntryDto, AuditLogEntry> {
 }
